ad0e057ac1c29262b6479e55941724486718ef90
w13b3 <wiebe@email.com>
2021-06-26 18:50:49 +0200

    add abd __init__

---
+++ abd/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# abd/__init__.py
+
+__all__ = ['ABD', 'AbstractBaseDecorator', 'BaseDecorator']
+
+from abd.abd import ABD, AbstractBaseDecorator, BaseDecorator

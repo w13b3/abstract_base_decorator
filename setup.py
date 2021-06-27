c9f0f916848a56dd92402b99c845bdace08b9183
w13b3 <wiebe@email.com>
2021-06-27 21:04:51 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,42 +1,160 @@
-import pathlib
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# setup.py
 
-# The directory containing this file
-here = pathlib.Path(__file__).parent.resolve()
+# requirements:
+#  python -m pip install wheel setuptools
 
-# The text of the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+# Usage:
+#  python setup.py create
 
-# This call to setup() does all the work
+# install wheel
+#   pip install ./dist/*.whl
+
+# unittest
+#  python -m unittest discover --catch --verbose
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+""" Project structure
+.                               # project directory
+├── <your Python project>       # name of your project
+│   ├── __init__.py             # indicates to Python this is a package
+│   ├── __version__.py          # optional
+│   ├── <project code>.py   
+│   └── ...
+├── doc
+|   └── ...
+├── test
+│   └── <test_project code>.py  # create a test
+│   └── ...
+├── LICENSE.txt
+├── MANIFEST.in
+├── pyproject.toml
+├── README.md                   # good description of the project
+└── setup.py                    # this file
+"""
+
+# Package meta-data.
+NAME = 'abd'
+DESCRIPTION = 'Abstract base class for decorators'
+URL = 'https://github.com/w13b3/abstract_base_decorator'
+EMAIL = 'wiebe at email dot com'  # prevent warning 'missing meta-data'
+AUTHOR = 'wiebe'
+REQUIRES_PYTHON = '>=3.6.0, <4'
+VERSION = ''  # change this
+# If you do change the License, remember to change the Trove Classifier for that!
+LICENSE = "Mozilla Public License Version 2.0"
+
+# If your package is a single module, use this instead of 'packages':
+PY_MODULES = [
+    'abd'
+]
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # None
+]
+
+# What packages are optional?
+EXTRAS = {
+    # None
+}
+# Trove Classifiers
+CLASSIFIERS = [  # https://pypi.org/classifiers/
+    "Development Status :: 2 - Pre-Alpha",
+    "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3 :: Only",
+    "Typing :: Typed",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Build Tools",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+keywords = ["decorator abstract oop"],
+
+if not bool(VERSION) and len(VERSION) <= 0:
+    raise Exception("Forgot to change the version")
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+class CreateCommand(Command):
+    """Support setup.py create."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…\n')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("\nReady to upload to PyPI.")
+        sys.exit()
+
+
 setup(
-    name="abd",
-    version="0.0.1",
-    description="Abstract base class for decorators",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/w13b3/abstract_base_decorator",
-    author="wiebe",
-    license="Mozilla Public License Version 2.0",
-    python_requires='>=3.6.*, <4',
-    classifiers=[  # https://pypi.org/classifiers/
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3 :: Only",
-        "Typing :: Typed",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords=["decorator abstract oop"],
-    package_dir={'': 'abd'},
-    packages=find_packages("", exclude=["/test"]),
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=[
+        "test",  "*.test",  "*.test.*",  "test.*",
+        "tests", "*.tests", "*.tests.*", "tests.*",
+    ]),
+    py_modules=PY_MODULES,
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    # $ setup.py create support.
+    cmdclass={
+        'create': CreateCommand,
+    },
 )
